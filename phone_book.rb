fad8e570bf3d0b16f5b82a1e2eb6d406edcd5a05
Eve <eve.adele.asher@gmail.com>
2014-02-25 10:07:22 -0500

    Add ability to add contact

--- phone_book.rb
+++ phone_book.rb
@@ -1,2 +1,12 @@
-print "What is the person's name? "
-gets.chomp
+contact_list = []
+puts "What is the person's first name? "
+first_name = gets.chomp
+puts "What is the person's last name?"
+last_name = gets.chomp
+puts "What is the person's phone number?"
+phone_number = gets.chomp
+puts "What is the person's email address?"
+email_address = gets.chomp
+contact_list << {first_name: first_name,
+  last_name: last_name, phone_number: phone_number,
+  email_address: email_address}
